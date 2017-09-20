1e53e9418029e2384e20a31d9d65d92f69d4b252
wskeee <361733529@qq.com>
2017-09-20 13:47:12 +0800

    修改backend 的user模块，改名为user_admin

--- environments/prod/backend/config/main-local.php
+++ environments/prod/backend/config/main-local.php
@@ -22,7 +22,6 @@ return [
             'gii/*',
             'debug/*',
             'user_admin/*',
-            'study/*',
             // The actions listed here will be allowed to everyone including guests.
             // So, 'admin/*' should not appear here in the production, of course.
             // But in the earlier stages of your development, you may probably want to
