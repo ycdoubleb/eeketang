9ec2c4cf317992ca0de806fd8082ee64da7b1d21
Kiwi° <Kiwi@Kiwi>
2017-10-13 09:58:15 +0800

    添加学院

--- frontend/modules/study/views/default/index.php
+++ frontend/modules/study/views/default/index.php
@@ -1,128 +1,139 @@
 <?php
 
+use common\models\course\Course;
 use frontend\modules\study\assets\StudyAsset;
-use wskeee\utils\ArrayUtil;
 use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
 use yii\helpers\Url;
 use yii\web\View;
+use yii\widgets\ActiveForm;
 
 /* @var $this View */
 
 $this->title = Yii::t('app', 'My Yii Application');
+
 ?>
 
-<div class="study-default-index _index">
-
-    <div class="banner">
-        <div class="banner-background">
-            <?php //echo Html::img(['/filedata/site/image/background.jpg'], ['class' => 'background-img']) ?>
+<div class="study-default-index index">
+    
+    <div class="banner"></div>
+    <div class="search" id="scroll">
+        <div class="container">
+            <div class="search-inner">
+                <div class="search-input">
+                    <?php $form = ActiveForm::begin([
+                        'id' => 'search-form',
+                        'action' => ['search', 'par_id' => $filter['par_id']],
+                        'method' => 'get'
+                    ]); ?>
+                    <?= Html::textInput('keyword', ArrayHelper::getValue($filter, 'keyword'), ['class' => 'form-control', 'placeholder' => '请输入你想搜索的关键词']) ?>
+                    <?php ActiveForm::end() ?>
+                </div>
+                <div class="search-button"><i class="icon icon-1"></i></div>
+            </div>
         </div>
     </div>
-
-    <div id="scroll" class="body-content">
-        <div class="row">
+    <div class="content">
+        <div class="container">
             <!--面包屑-->
-            <div class="crumbs-bar">
-                <div class="cb-nav">
-                    <div class="cbn-item">
-                        <span>筛选条件：</span>
-                        <b><?= $category->name ?></b>
-                    </div>
-                    
-                    <?php foreach ($filters as $filter_name => $filter_value): ?>
-                    <div class="cbn-item">
-                        <i class="cbni-arrow">&gt;</i>
-                        <?= Html::a("<b>{$filter_name}：</b><em>{$filter_value['filter_value']}</em><i>×</i>", $filter_value['url'], ['class' => 'cnbi-key']) ?>
-                    </div>
-                   <?php endforeach; ?>
-                    
-                </div>
+            <div class="crumb">
+                <ul class="crumb-nav">
+                    <li><span>筛选条件：</span><b>广州特色资源库</b></li>
+                    <li>
+                        <i class="arrow">&gt;</i>
+                        <a><b>分类:</b><em>小学</em><i>×</i></a>
+                    </li>
+                </ul>
             </div>
-            <!--面包屑-->
-            <!--条件选择-->
-            <div class="selector-column">
-                
-                <?php if (!isset($filter['cat_id']) && count($result['cats'])>0): ?>
-                    <div class="sc-attribute">
-                        <div class="sc-key">
-                            <span><?= Yii::t('app', 'Cat') ?>：</span>
-                        </div>
-                        <div class="sc-value">
-                            <ul>
-                                <?php foreach ($result['cats'] as $cat): ?>
-                                    <li><?= Html::a($cat['name'], Url::to(array_merge(['index'], array_merge($filter, ['cat_id' => $cat['id'], 'page' => 1])))) ?></li>
-                                <?php endforeach; ?>
-                            </ul>
-                        </div>
-                    </div>
-                <?php endif; ?>
-
-                <?php foreach ($result['attrs'] as $attr_name => $attr_arr):?>
-                    <div class="sc-attribute">
-                        <div class="sc-key">
-                            <span><?= $attr_name ?>：</span>
-                        </div>
-                        <div class="sc-value">
-                            <ul>
-                                <?php $attr_arr['value'] = ArrayUtil::sortCN($attr_arr['value']);?>
-                                <?php foreach ($attr_arr['value'] as $attr_label): ?>
-                                    
-                                    <li>
-                                    <?php
-                                        //合并之前已选择的属性过滤条件
-                                        $attrs = array_merge(isset($filter['attrs']) ? $filter['attrs'] : [] , [['attr_id' => $attr_arr['attr_id'], 'attr_value' => $attr_label]]);
-                                        //过滤之前已选择过滤条件
-                                        $params = array_merge($filter,['attrs' => $attrs, 'page' => 1]);
-                                        echo Html::a($attr_label, Url::to(array_merge(['index'], $params)))
-                                    ?>
-                                    </li>
-                                    
-                                <?php endforeach; ?>
-                            </ul>
-                        </div>
-                    </div>
-                <?php endforeach; ?>
-
+            <!--属性选择-->
+            <div class="attribute">
+                <ul class="attr-nav">
+                    <?php if(!isset($filter['cat_id']) && count($results['category']) > 1): ?>
+                    <li>
+                        <span class="attr-key"><?= Yii::t('app', 'Category') ?>：</span>
+                        <ul class="attr-value">
+                            <?php foreach ($results['category'] as $category): ?>
+                            <li><?= Html::a($category['name'], Url::to(array_merge(['index'], array_merge($filter, ['cat_id' => $category['id'],'#'=>'scroll'])))) ?></a></li>
+                            <?php endforeach; ?>
+                        </ul>
+                    </li>
+                    <?php endif; ?>
+                    <?php if(!isset($filter['sub_id']) && count($results['subject']) > 1): ?>
+                    <li>
+                        <span class="attr-key"><?= Yii::t('app', 'Subject') ?>：</span>
+                        <ul class="attr-value">
+                            <?php foreach ($results['subject'] as $subject): ?>
+                            <li><?= Html::a($subject['name'], Url::to(array_merge(['index'], array_merge($filter, ['sub_id' => $subject['id'],'#'=>'scroll'])))) ?></li>
+                            <?php endforeach; ?>
+                        </ul>
+                    </li>
+                    <?php endif; ?>
+                    <?php if(!isset($filter['term']) && count(array_filter($results['term'])) > 1): ?>
+                    <li>
+                        <span class="attr-key"><?= Yii::t('app', 'Term') ?>：</span>
+                        <ul class="attr-value">
+                            <?php asort($results['term']) ?>
+                            <?php foreach ($results['term'] as $term): ?>
+                            <li><?= Html::a(Course::$term_keys[$term], Url::to(array_merge(['index'], array_merge($filter, ['term' => $term,'#'=>'scroll'])))) ?></li>
+                            <?php endforeach; ?>
+                        </ul>
+                    </li>
+                    <?php endif; ?>
+                    <?php if(!isset($filter['grade']) && count(array_filter($results['grade'])) > 1): ?>
+                    <li>
+                        <span class="attr-key"><?= Yii::t('app', 'Grade') ?>：</span>
+                        <ul class="attr-value">
+                            <?php asort($results['grade']) ?>
+                            <?php foreach ($results['grade'] as $grade): ?>
+                            <li><?= Html::a(Course::$grade_keys[$grade], Url::to(array_merge(['index'], array_merge($filter, ['grade' => $grade,'#'=>'scroll'])))) ?></li>
+                            <?php endforeach; ?>
+                        </ul>
+                    </li>
+                    <?php endif; ?>
+                    <?php if(!isset($filter['tm_ver']) && count(array_filter($results['tm_ver'])) > 1): ?>
+                    <li>
+                        <span class="attr-key"><?= Yii::t('app', 'Teaching Material Version') ?>：</span>
+                        <ul class="attr-value">
+                            <?php foreach ($results['tm_ver'] as $tm_ver): ?>
+                            <li><?= Html::a($tm_ver, Url::to(array_merge(['index'], array_merge($filter, ['tm_ver' => $tm_ver,'#'=>'scroll'])))) ?></li>
+                            <?php endforeach; ?>
+                        </ul>
+                    </li>
+                    <?php endif; ?>
+                </ul>
             </div>
-            <!--条件选择-->
             <!--过滤器-->
             <?= $this->render('/layouts/filter', ['filter' => $filter]) ?>
-            <!--过滤器-->
             <!--课程课件-->
-            <div class="goods-column">
-                <?php foreach ($result['courses'] as $index => $course):?>
-                
-                <div class="<?= ($index % 5 == 4 ) ? 'none-margin' : 'gc-item'; ?>">
-                    <?= Html::a('<div class="gc-img">'.Html::img([$course['img']], ['width' => '100%']).'</div>', ['view', 'parent_cat_id' => $course['parent_cat_id'], 'cat_id' => $course['cat_id'], 'id' => $course['id']], ['title' => "【{$course['unit']}】{$course['name']}" ]) ?>
-                    <div class="gc-name course-name"><?= "【{$course['unit']}】{$course['name']}" ?></div>
-                    <div class="gc-see">
+            <div class="goods">
+                <?php foreach ($results['courses'] as $index => $courses): ?>
+                <div class="<?= ($index % 5 == 4 ) ? 'goods-item none' : 'goods-item'; ?>">
+                    <?= Html::a('<div class="goods-img"></div>', ['view', 'id' => $courses['id']], ['title' => "【{$courses['unit']}】{$courses['cour_name']}" ]) ?>
+                    <div class="goods-name course-name"><?= "【{$courses['unit']}】{$courses['cour_name']}" ?></div>
+                    <div class="goods-see">
                         <i class="glyphicon glyphicon-play-circle"></i>
-                        <span><?= $course['play_count'] <= 99999 ? number_format($course['play_count']) : substr(number_format((($course['play_count']/10000)*10)/10, 4),0,-3).'万'; ?></span>
+                        <span><?= $courses['play_count'] <= 99999 ? number_format($courses['play_count']) : substr(number_format((($courses['play_count']/10000)*10)/10, 4),0,-3).'万'; ?></span>
                     </div>
                 </div>
                 <?php endforeach; ?>
             </div>
-            <!--课程课件-->
             <!--分页-->
             <?= $this->render('/layouts/page', ['filter' => $filter, 'pages' => $pages]) ?>
-            <!--分页-->
-
-        </div>   
-
+        </div>
     </div>
-
 </div>
 
 <?php
-$subject = ArrayHelper::getValue($filter, 'parent_cat_id');
-$scroll = isset($filter['cat_id']) || isset($filter['attrs']) || isset($filter['page']) ? 1 : 0;
+$par_id = ArrayHelper::getValue($filter, 'par_id');
+//$scroll = isset($filter['cat_id']) || isset($filter['sub_id']) 
+//        || isset($filter['term']) || isset($filter['grade'])
+//        || isset($filter['tm_ver']) || isset($filter['page']) ? 1 : 0;
 $js = <<<JS
-    var subjectArray = new Array("sites", "yellow", "green", "blue", "purple", "brown");
-    $("body").addClass(subjectArray[$subject]);
+    var subjectArray = {4:"guangzhou"};        
+    $(".index").addClass(subjectArray[$par_id]);
         
-    if($scroll) 
-        window.location.hash = "#scroll"; 
+//    if(1) 
+//        location.hash = "scroll"; 
 JS;
     $this->registerJs($js, View::POS_READY);
 ?>
--- frontend/modules/study/views/layouts/page.php
+++ frontend/modules/study/views/layouts/page.php
@@ -11,24 +11,22 @@ use yii\widgets\LinkPager;
 /* @var $pages Pagination */
 
 //$this->title = Yii::t('app', 'My Yii Application');
-//$prevLinks = Url::to(array_merge(['index'], array_merge($filter, ['page' => $pages->page])));
-//$nextLinks = Url::to(array_merge(['index'], array_merge($filter, ['page' => $pages->page+2])));
 ?>
 
 <div class="page">
-    <div class="p-wrap">
-        <div class="p-num">
+    <div class="page-wrap">
+        <div class="page-num">
             <?= LinkPager::widget([
                 'pagination' => $pages,
                 'options' => ['class' => 'pagination', 'style' => 'margin: 0px;border-radius: 0px;'],
-                'prevPageCssClass' => 'pn-prev',
-                'nextPageCssClass' => 'pn-next',
+                'prevPageCssClass' => 'page-prev',
+                'nextPageCssClass' => 'page-next',
                 'prevPageLabel' => '<i>&lt;</i>'.Yii::t('app', 'Prev Page'),
                 'nextPageLabel' => Yii::t('app', 'Next Page').'<i>&gt;</i>',
                 'maxButtonCount' => 5,
             ]); ?>
         </div>
-        <div class="p-skip">
+        <div class="page-skip">
             <?php if($pages->pageCount >= 2): ?>
             共<b><?= $pages->pageCount; ?></b>页&nbsp;&nbsp;到第<?= Html::textInput('page', $pages->page+1, ['class' => 'input-txt']) ?>页
             <?= Html::a(Yii::t('app', 'Save'), "javascript:;", ['id' => 'submit', 'class' => 'btn btn-default']) ?>
