55785e79ed9eabd23c6f7a49fabda05710d1b08c
wskeee <361733529@qq.com>
2017-11-30 11:06:47 +0800

    1、修改页脚

--- frontend/views/layouts/footer.php
+++ frontend/views/layouts/footer.php
@@ -10,8 +10,8 @@ use yii\web\View;
 <footer class="footer">
     <div class="container">
         <div class="pull-left">
-            Copyright &copy; 2017 eeketang.com&nbsp;&nbsp;ee课堂&nbsp;&nbsp;版权所有&nbsp;&nbsp;
-            广州远程教育中心有限公司<br/><span>战略合作&nbsp;&nbsp;视觉中国</span>
+            Copyright &copy; 2017 www.eeketang.com&nbsp;&nbsp;ee课堂&nbsp;&nbsp;版权所有&nbsp;&nbsp;
+            广州远程教育中心有限公司<br/><?=Html::a('备案号粤ICP备17135407号-2','http://www.miitbeian.gov.cn/')?> <span>战略合作&nbsp;&nbsp;视觉中国</span>
         </div>
         <div class="pull-right">
             <?= Html::a(Html::img(['/filedata/site/image/eeketang.png']), Url::to("http://eeketang.gzedu.com/")) ?>
